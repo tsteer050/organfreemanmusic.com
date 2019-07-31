679d424c0886cd559013c6575afae788da42b333
tsteer <trevor.piano@gmail.com>
2019-07-31 09:37:17 -0700

    Add splash page and title

---
+++ src/Splash.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Splash</h1>
+      </div>
+    );
+  }
+}
+
+export default Splash;

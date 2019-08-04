f2a47e4d7f57c1d636e7bd932349b4436c426965
tsteer <trevor.piano@gmail.com>
2019-08-03 23:16:21 -0700

    Add navigation router links

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './menubar.css';
 
 class MenuBar extends React.Component {
@@ -10,24 +11,34 @@ class MenuBar extends React.Component {
     return (
       <div className="menu-bar">
         <ul className="menu-bar-items">
-          <li>
-            <h1>Music</h1>
-          </li>
-          <li>
-            <h1>Tour</h1>
-          </li>
-          <li>
-            <h1>Band</h1>
-          </li>
+          <Link to="/music">
+            <li>
+              <h1>Music</h1>
+            </li>
+          </Link>
+          <Link to="/tour">
+            <li>
+              <h1>Tour</h1>
+            </li>
+          </Link>
+          <Link to="/band">
+            <li>
+              <h1>Band</h1>
+            </li>
+          </Link>
           <li>
             <a href="https://organfreemanmusic.hilinemerchandising.com/">Store</a>
           </li>
-          <li>
-            <h1>Subscribe</h1>
-          </li>
-          <li>
-            <h1>Contact</h1>
-          </li>
+          <Link to="/subscribe">
+            <li>
+              <h1>Subscribe</h1>
+            </li>
+          </Link>
+          <Link to="/contact">
+            <li>
+              <h1>Contact</h1>
+            </li>
+          </Link>
         </ul>
       </div>
     )
